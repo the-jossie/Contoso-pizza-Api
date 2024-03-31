7faadfb678d0d5724a080777b8b851027ddfa6cd
Dev Jo <idowuayanfeoluwa@gmail.com>
2024-03-31 19:30:45 +0100

    feat: hhtp test the pizza endpoints

--- obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+517f93ca6c06ba6cf415fdc9b5a1fb7e594918b7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6896559a7f0c918a590025dd95457796b4230100")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
